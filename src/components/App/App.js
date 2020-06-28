35b6e63f25bc1f6b4de350d5ce6e902991021e93
Christopher Neisen <christopherjay71186@gmail.com>
2020-06-28 11:14:55 -0500

    comments for header done

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -9,7 +9,9 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        {/* everything but the footer lives in the MovieList component */}
         <MovieList />
+        {/* component for the footer */}
         <Footer />
       </div>
     );
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,21 +3,20 @@ import { connect } from "react-redux";
 
 // Header is a controlled component that renders the header of the site
 class Header extends Component {
+  //generates and random number used to select a random photo, random multiplied by the amount of movies in database
     randomNum = () => {
   let newNum = Math.floor(Math.random() * this.props.reduxState.movies.length);
   return newNum;
 } //end randomNum
   // React render function
-
-
-
   render() {
+    //resets a new random number every 20 seconds and forces a rerender of the header
      setInterval(() => {
         this.randomNum();
        this.forceUpdate();
         }, 20000);
     
-        
+        //value saved to varible for later use
       let newNum = this.randomNum()
     return (
       <div className="App">
@@ -25,10 +24,14 @@ class Header extends Component {
           <br />
           <h1 className="App-title">Now Showing:</h1>
           <div className="photoGallery">
+            {/* map through reduxState.movies */}
             {this.props.reduxState.movies.map((movie) => {
+              //...if the index of the array matches the random number...
               return this.props.reduxState.movies.indexOf(movie) === newNum ? (
+                //...display the image...
                 <img src={movie.poster}></img>
               ) : (
+                //...else display nothing
                 <span></span>
               );
             })}
