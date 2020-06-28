641f101aab2a18c955efb403bf53ae3d34e7e3f9
Christopher Neisen <christopherjay71186@gmail.com>
2020-06-28 10:59:12 -0500

    server comments done

--- server/routes/movierouter.js
+++ server/routes/movierouter.js
@@ -4,6 +4,7 @@ const router = express.Router();
 
 //get all the stored movies
 router.get('/', (req, res) => {
+  //selects everything we need from the database
     pool
       .query('select movies.title, array_agg(genre_name) as genres, array_agg(distinct movies.id) as id, array_agg(distinct movies.poster) as poster, array_agg(distinct movies.description) as description from movies JOIN "junction" on "junction"."movie_id" = "movies"."id" JOIN "genres" on "junction"."genre_id" = "genres"."id" group by "title" ORDER BY id;')
         .then((result) => {
@@ -11,11 +12,12 @@ router.get('/', (req, res) => {
             console.log("res.send", result)
         })
         .catch((error) => {
-            console.log("Error GET /movie", error);
-            res.sendStatus(500);
+            console.log("Error GET /movie", error); 
+            res.sendStatus(500); // 500 error
         });
 }) //end GET
 
+//saves edited movie values to database
 router.put("/:id", (req, res) => {
   let id = req.params.id; // grabs id and places it in path
   const movie = req.body; // pull the object out out of the HTTP REQUEST
@@ -25,9 +27,9 @@ if (movie === undefined) {
   // stop, dont touch the database
   res.sendStatus(400); // 400 BAD REQUEST
   return;
-} //POST end
+}
   let queryText = `UPDATE movies SET title = $1, description = $2 WHERE id = $3`;
-  //....and uopdates it with put to flagged
+  //....updates database with edited values
   pool
     .query(queryText, [title, description, id])
 
